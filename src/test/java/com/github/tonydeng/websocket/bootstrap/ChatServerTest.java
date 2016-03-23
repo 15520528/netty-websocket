c45277ce5725482da0f00acc751f96b5b2545e92
Tony Deng <wolf.deng@gmail.com>
2016-03-23 15:32:49 +0800

    完成简单的聊天功能

---
+++ src/test/java/com/github/tonydeng/websocket/bootstrap/ChatServerTest.java
@@ -0,0 +1,38 @@
+package com.github.tonydeng.websocket.bootstrap;
+
+import com.google.common.base.Joiner;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.util.StringJoiner;
+
+/**
+ * Created by tonydeng on 16/3/23.
+ */
+public class ChatServerTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ChatServerTest.class);
+
+//    @Test
+    public void testGetPath() throws FileNotFoundException {
+
+        RandomAccessFile file = new RandomAccessFile(
+                Joiner.on(File.separator)
+                        .join(new String[]{System.getProperty("user.dir"),"src","main","resources","index.html"}),
+                "r"
+        );
+        if(log.isInfoEnabled())
+            log.info("user dir path:'{}'   html:'{}'",System.getProperty("user.dir"),file);
+
+    }
+
+    @Test
+    public void testMain() {
+        ChatServer server = new ChatServer();
+        server.main(new String[0]);
+    }
+}
